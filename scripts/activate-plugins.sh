6223221a364af072ecd92fa413a382936eda9ee7
Hollocombe, Jonathan <jonathan.hollocombe@ukaea.uk>
2018-09-21 15:52:47 +0100

    Generating active and remove plugins scripts during CMake configuration.

--- scripts/activate-plugins.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-UDA_HOME=$HOME/uda
-
-$UDA_HOME/bin/install_plugin -u $UDA_HOME install exp2imas
-#$UDA_HOME/bin/install_plugin -u $UDA_HOME install imas_mapping
-#$UDA_HOME/bin/install_plugin -u $UDA_HOME install imasdd
