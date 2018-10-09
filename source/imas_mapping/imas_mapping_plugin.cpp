d42c25d416b60f6795cad7273b36240cb5453d20
Jonathan Hollocombe <jonathan.hollocombe@ukaea.uk>
2018-10-09 20:34:55 +0100

    updating IMAS_MAPPING plugin

--- source/imas_mapping/imas_mapping_plugin.cpp
+++ source/imas_mapping/imas_mapping_plugin.cpp
@@ -3,6 +3,7 @@
 
 #include <string>
 #include <sstream>
+#include <stack>
 #include <vector>
 #include <stack>
 #include <iterator>
@@ -23,6 +24,20 @@
 
 namespace {
 
+struct Pulse {
+    const int shot;
+    const int run;
+    const std::string user;
+    const std::string tokamak;
+    const std::string version;
+
+    Pulse(int shot_, int run_, std::string user_, std::string tokamak_, std::string version_)
+            : shot(shot_), run(run_), user(std::move(user_)), tokamak(std::move(tokamak_)), version(std::move(version_))
+    {}
+
+    Pulse(const Pulse& other) = default;
+};
+
 class MappingPlugin {
 public:
     MappingPlugin() = default;
@@ -30,18 +45,28 @@ public:
     void init() {}
     void reset() {}
 
-    int help(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int version(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int build_date(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int default_method(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int max_interface_version(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int open(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int close(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int get(IDAM_PLUGIN_INTERFACE* plugin_interface);
-    int getdim(IDAM_PLUGIN_INTERFACE* plugin_interface);
+    int help(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int version(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int build_date(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int default_method(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int max_interface_version(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int open_pulse(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int close_pulse(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int begin_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int end_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int write_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int read_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int delete_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int begin_arraystruct_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+
+    int get(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
+    int get_dim(IDAM_PLUGIN_INTERFACE* idam_plugin_interface);
 
 private:
     uda::imas_mapping::MachineMapping machine_mapping_;
+    std::vector<std::string> path_stack_;
+    std::unordered_map<int, Pulse> pulses_;
+    int next_pulse_ = 0;
 };
 
 } // anon namespace
@@ -59,35 +84,49 @@ int imas_mapping_plugin(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 
         REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
 
-        if (idam_plugin_interface->housekeeping || STR_IEQUALS(request_block->function, "reset")) {
+        std::string function = request_block->function;
+
+        if (idam_plugin_interface->housekeeping || function == "reset") {
             plugin.reset();
             return 0;
         }
 
         plugin.init();
 
-        if (STR_IEQUALS(request_block->function, "init") || STR_IEQUALS(request_block->function, "initialise")) {
+        if (function == "init" || function == "initialise") {
             return 0;
         }
 
-        if (STR_IEQUALS(request_block->function, "help")) {
+        if (function == "help") {
             return plugin.help(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "version")) {
+        } else if (function == "version") {
             return plugin.version(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "builddate")) {
+        } else if (function == "builddate") {
             return plugin.build_date(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "defaultmethod")) {
+        } else if (function == "defaultmethod") {
             return plugin.default_method(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "maxinterfaceversion")) {
+        } else if (function == "maxinterfaceversion") {
             return plugin.max_interface_version(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "open")) {
-            return plugin.open(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "close")) {
-            return plugin.close(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "get")) {
+        } else if (function == "openPulse") {
+            return plugin.open_pulse(idam_plugin_interface);
+        } else if (function == "closePulse") {
+            return plugin.close_pulse(idam_plugin_interface);
+        } else if (function == "beginAction") {
+            return plugin.begin_action(idam_plugin_interface);
+        } else if (function == "endAction") {
+            return plugin.end_action(idam_plugin_interface);
+        } else if (function == "writeData") {
+            return plugin.write_data(idam_plugin_interface);
+        } else if (function == "readData") {
+            return plugin.read_data(idam_plugin_interface);
+        } else if (function == "deleteData") {
+            return plugin.delete_data(idam_plugin_interface);
+        } else if (function == "beginArraystructAction") {
+            return plugin.begin_arraystruct_action(idam_plugin_interface);
+        } else if (function == "get") {
             return plugin.get(idam_plugin_interface);
-        } else if (STR_IEQUALS(request_block->function, "getdim")) {
-            return plugin.getdim(idam_plugin_interface);
+        } else if (function == "getDim") {
+            return plugin.get_dim(idam_plugin_interface);
         } else {
             RAISE_PLUGIN_ERROR("Unknown function requested!");
         }
@@ -103,12 +142,12 @@ namespace {
  * @param idam_plugin_interface
  * @return
  */
-int MappingPlugin::help(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::help(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
     const char* help = "\ntemplatePlugin: Add Functions Names, Syntax, and Descriptions\n\n";
     const char* desc = "templatePlugin: help = description of this plugin";
 
-    return setReturnDataString(plugin_interface->data_block, help, desc);
+    return setReturnDataString(idam_plugin_interface->data_block, help, desc);
 }
 
 /**
@@ -116,9 +155,9 @@ int MappingPlugin::help(IDAM_PLUGIN_INTERFACE* plugin_interface)
  * @param idam_plugin_interface
  * @return
  */
-int MappingPlugin::version(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::version(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    return setReturnDataIntScalar(plugin_interface->data_block, THISPLUGIN_VERSION, "Plugin version number");
+    return setReturnDataIntScalar(idam_plugin_interface->data_block, THISPLUGIN_VERSION, "Plugin version number");
 }
 
 /**
@@ -126,9 +165,9 @@ int MappingPlugin::version(IDAM_PLUGIN_INTERFACE* plugin_interface)
  * @param idam_plugin_interface
  * @return
  */
-int MappingPlugin::build_date(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::build_date(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    return setReturnDataString(plugin_interface->data_block, __DATE__, "Plugin build date");
+    return setReturnDataString(idam_plugin_interface->data_block, __DATE__, "Plugin build date");
 }
 
 /**
@@ -136,9 +175,9 @@ int MappingPlugin::build_date(IDAM_PLUGIN_INTERFACE* plugin_interface)
  * @param idam_plugin_interface
  * @return
  */
-int MappingPlugin::default_method(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::default_method(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    return setReturnDataString(plugin_interface->data_block, THISPLUGIN_DEFAULT_METHOD, "Plugin default method");
+    return setReturnDataString(idam_plugin_interface->data_block, THISPLUGIN_DEFAULT_METHOD, "Plugin default method");
 }
 
 /**
@@ -146,15 +185,15 @@ int MappingPlugin::default_method(IDAM_PLUGIN_INTERFACE* plugin_interface)
  * @param idam_plugin_interface
  * @return
  */
-int MappingPlugin::max_interface_version(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::max_interface_version(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    return setReturnDataIntScalar(plugin_interface->data_block, THISPLUGIN_MAX_INTERFACE_VERSION,
+    return setReturnDataIntScalar(idam_plugin_interface->data_block, THISPLUGIN_MAX_INTERFACE_VERSION,
                                   "Maximum Interface Version");
 }
 
-int MappingPlugin::open(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::open_pulse(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    REQUEST_BLOCK* request_block = plugin_interface->request_block;
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
 
     int shot;
     FIND_REQUIRED_INT_VALUE(request_block->nameValueList, shot);
@@ -162,20 +201,201 @@ int MappingPlugin::open(IDAM_PLUGIN_INTERFACE* plugin_interface)
     int run;
     FIND_REQUIRED_INT_VALUE(request_block->nameValueList, run);
 
+    const char* user;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, user);
+
+    const char* tokamak;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, tokamak);
+
+    const char* version;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, version);
+
+    pulses_.emplace(next_pulse_, Pulse{ shot, run, user, tokamak, version });
+
+    setReturnDataIntScalar(idam_plugin_interface->data_block, next_pulse_, nullptr);
+
+    ++next_pulse_;
     return 0;
 }
 
-int MappingPlugin::close(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::close_pulse(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    REQUEST_BLOCK* request_block = plugin_interface->request_block;
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
 
-    int shot;
-    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, shot);
+    int ctxId;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, ctxId);
 
-    int run;
-    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, run);
+    pulses_.erase(ctxId);
 
+    setReturnDataIntScalar(idam_plugin_interface->data_block, ctxId, nullptr);
     return 0;
+}
+
+int MappingPlugin::begin_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
+
+    int ctxId;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, ctxId);
+
+    const char* dataObject;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, dataObject);
+
+    int access;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, access);
+
+    int range;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, range);
+
+#ifdef FIND_REQUIRED_DOUBLE_VALUE
+    double time;
+    FIND_REQUIRED_DOUBLE_VALUE(request_block->nameValueList, time);
+#else
+    float time;
+    FIND_REQUIRED_FLOAT_VALUE(request_block->nameValueList, time);
+#endif
+
+    int interp;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, interp);
+
+    if (pulses_.count(ctxId) == 0) {
+        RAISE_PLUGIN_ERROR("invalid ctxId");
+    }
+
+    path_stack_.emplace_back(dataObject);
+
+    setReturnDataIntScalar(idam_plugin_interface->data_block, ctxId, nullptr);
+    return 0;
+}
+
+int MappingPlugin::end_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
+
+    int ctxId;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, ctxId);
+
+    int type;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, type);
+
+    if (pulses_.count(ctxId) == 0) {
+        RAISE_PLUGIN_ERROR("invalid ctxId");
+    }
+
+    path_stack_.pop_back();
+
+    setReturnDataIntScalar(idam_plugin_interface->data_block, ctxId, nullptr);
+    return 0;
+}
+
+int MappingPlugin::write_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    RAISE_PLUGIN_ERROR("cannot write data with mapping plugin");
+}
+
+int MappingPlugin::delete_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    RAISE_PLUGIN_ERROR("cannot delete data with mapping plugin");
+}
+
+bool is_integer(const std::string& string)
+{
+    if (string.empty()) {
+        return false;
+    }
+    char* end = nullptr;
+    std::strtol(string.c_str(), &end, 10);
+    return end != nullptr && *end == '\0';
+}
+
+int MappingPlugin::begin_arraystruct_action(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
+
+    int ctxId;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, ctxId);
+
+    int size;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, size);
+
+    const char* dataObject;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, dataObject);
+
+    int access;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, access);
+
+    int range;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, range);
+
+#ifdef FIND_REQUIRED_DOUBLE_VALUE
+    double time;
+    FIND_REQUIRED_DOUBLE_VALUE(request_block->nameValueList, time);
+#else
+    float time;
+    FIND_REQUIRED_FLOAT_VALUE(request_block->nameValueList, time);
+#endif
+
+    int interp;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, interp);
+
+    const char* path;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, path);
+
+    const char* timebase;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, timebase);
+
+    if (pulses_.count(ctxId) == 0) {
+        RAISE_PLUGIN_ERROR("invalid ctxId");
+    }
+
+    Pulse pulse = pulses_.find(ctxId)->second;
+
+    std::vector<std::string> tokens;
+
+    std::stringstream ss(path);
+    std::string fragment;
+    while (std::getline(ss, fragment, '/')) {
+        tokens.push_back(fragment);
+    }
+
+    int pos = 1;
+    while (pos < (path_stack_.size() - 1) && pos < tokens.size() && path_stack_[pos] == tokens[pos - 1]) {
+        ++pos;
+    }
+
+    while (path_stack_.size() != pos) {
+        path_stack_.pop_back();
+    }
+
+    std::copy(tokens.begin() + (pos - 1), tokens.end(), std::back_inserter(path_stack_));
+
+    auto plugin_name = machine_mapping_.plugin(pulse.tokamak);
+
+    int uda_type = UDA_TYPE_INT;
+
+    std::stringstream element_ss;
+    std::stringstream indices_ss;
+
+    const char* element_delim = "";
+    const char* indices_delim = "";
+
+    for (const auto& el : path_stack_) {
+        if (is_integer(el)) {
+            element_ss << element_delim << "#";
+            indices_ss << indices_delim << el;
+            indices_delim = ",";
+        } else {
+            element_ss << element_delim << el;
+        }
+        element_delim = "/";
+    }
+
+    auto request =
+            boost::format("%s::read(experiment='%s', element='%s', shot=%d, indices='%s', dtype=%d, IDS_version='')")
+            % plugin_name % pulse.tokamak % element_ss.str() % pulse.shot % indices_ss.str() % uda_type;
+    std::cout << request.str() << std::endl;
+
+    return callPlugin(idam_plugin_interface->pluginList, request.str().c_str(), idam_plugin_interface);
 }
 
 size_t extract_array_indices(const char* input, char** output, int** indices)
@@ -238,7 +458,7 @@ char* indices_to_string(const int* indices, size_t num_indices)
     return string;
 }
 
-int convert_IMAS_to_UDS_type(const std::string& type)
+int convert_IMAS_to_UDA_type(const std::string& type)
 {
     if (type == "integer") {
         return UDA_TYPE_INT;
@@ -253,9 +473,75 @@ int convert_IMAS_to_UDS_type(const std::string& type)
     }
 }
 
-int MappingPlugin::get(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int convert_IMAS_to_UDA_type(int type)
 {
-    REQUEST_BLOCK* request_block = plugin_interface->request_block;
+    if (type == 50) {
+        return UDA_TYPE_STRING;
+    } else if (type == 51) {
+        return UDA_TYPE_INT;
+    } else if (type == 52) {
+        return UDA_TYPE_DOUBLE;
+    } else {
+        return UDA_TYPE_UNKNOWN;
+    }
+}
+
+int MappingPlugin::read_data(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
+
+    int ctxId;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, ctxId);
+
+    const char* field;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, field);
+
+    const char* timebase;
+    FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, timebase);
+
+    int datatype;
+    FIND_REQUIRED_INT_VALUE(request_block->nameValueList, datatype);
+
+    if (pulses_.count(ctxId) == 0) {
+        RAISE_PLUGIN_ERROR("invalid ctxId");
+    }
+
+    Pulse pulse = pulses_.find(ctxId)->second;
+
+    auto plugin_name = machine_mapping_.plugin(pulse.tokamak);
+
+    std::stringstream element_ss;
+    std::stringstream indices_ss;
+
+    const char* element_delim = "";
+    const char* indices_delim = "";
+
+    for (const auto& el : path_stack_) {
+        if (is_integer(el)) {
+            element_ss << element_delim << "#";
+            indices_ss << indices_delim << el;
+            indices_delim = ",";
+        } else {
+            element_ss << element_delim << el;
+        }
+        element_delim = "/";
+    }
+
+    element_ss << "/" << field;
+
+    int uda_type = convert_IMAS_to_UDA_type(datatype);
+
+    auto request =
+            boost::format("%s::read(experiment='%s', element='%s', shot=%d, indices='%s', dtype=%d, IDS_version='')")
+            % plugin_name % pulse.tokamak % element_ss.str() % pulse.shot % indices_ss.str() % uda_type;
+    std::cout << request.str() << std::endl;
+
+    return callPlugin(idam_plugin_interface->pluginList, request.str().c_str(), idam_plugin_interface);
+}
+
+int MappingPlugin::get(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
+{
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
 
     const char* expName;
     FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, expName);
@@ -287,19 +573,19 @@ int MappingPlugin::get(IDAM_PLUGIN_INTERFACE* plugin_interface)
     size_t num_indices = extract_array_indices(element.c_str(), &path, &indices);
     char* indices_string = indices_to_string(indices, num_indices);
 
-    int uda_type = convert_IMAS_to_UDS_type(type);
+    int uda_type = convert_IMAS_to_UDA_type(type);
 
     auto request =
             boost::format("%s::read(experiment='%s', element='%s', shot=%d, indices='%s', dtype=%d, IDS_version='')")
             % plugin_name % expName % path % shot % indices_string % uda_type;
     std::cout << request.str() << std::endl;
 
-    return callPlugin(plugin_interface->pluginList, request.str().c_str(), plugin_interface);
+    return callPlugin(idam_plugin_interface->pluginList, request.str().c_str(), idam_plugin_interface);
 }
 
-int MappingPlugin::getdim(IDAM_PLUGIN_INTERFACE* plugin_interface)
+int MappingPlugin::get_dim(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 {
-    REQUEST_BLOCK* request_block = plugin_interface->request_block;
+    REQUEST_BLOCK* request_block = idam_plugin_interface->request_block;
 
     const char* expName;
     FIND_REQUIRED_STRING_VALUE(request_block->nameValueList, expName);
@@ -335,7 +621,7 @@ int MappingPlugin::getdim(IDAM_PLUGIN_INTERFACE* plugin_interface)
             % plugin_name % expName % path % shot % indices_string % uda_type;
     std::cout << request.str() << std::endl;
 
-    return callPlugin(plugin_interface->pluginList, request.str().c_str(), plugin_interface);
+    return callPlugin(idam_plugin_interface->pluginList, request.str().c_str(), idam_plugin_interface);
 }
 
 } // anon namespace
