32680a9163b278e254c6a6dd188fb649089a072d
Jonathan Hollocombe <jonathan.hollocombe@ccfe.ac.uk>
2018-11-30 13:41:40 +0000

    fixing build on Freia

--- scripts/cmake-freia.sh
+++ scripts/cmake-freia.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
 module purge
-module load imas-modules
-module load imas
-module load idl/08.3
-module load java/1.8
+module use /common/ETS/modules
+module load imas/3.20.0/ual/3.8.5
+# blitz needed to use imas pkg-config
+module load blitz
+module load blitz-devel
 
 export HDF5_USE_SHLIB=yes
 export BOOST_ROOT=/usr/local/depot/boost-1.60
 
-cmake3 -Bbuild_freia -H. -DCMAKE_BUILD_TYPE=Debug -DTARGET_TYPE=MAST \
-    -DCMAKE_INSTALL_PREFIX=${HOME}/freia \
-    -DSWIG_EXECUTABLE=/home/jholloc/freia/bin/swig \
+UDA_HOME=/common/projects/UDA/2.2.3
+export PKG_CONFIG_PATH=$UDA_HOME/lib/pkgconfig:$PKG_CONFIG_PATH
+
+cmake3 -Bbuild_freia -H. -DCMAKE_BUILD_TYPE=Debug \
+    -DCMAKE_INSTALL_PREFIX=$UDA_HOME \
+    -DLIBSSH_ROOT=/common/projects/UDA/libssh \
+    -DBUILD_PLUGINS="exp2imas;imasdd;source;mast_imas" \
     $*
