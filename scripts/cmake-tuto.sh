5017730574ae7827e42d3d64e7dc0a3846ba286b
Fleury <ludovic.fleury@cea.fr>
2019-01-14 09:30:36 +0100

    Improving uda-plugins cmake file

--- scripts/cmake-tuto.sh
+++ scripts/cmake-tuto.sh
@@ -5,8 +5,9 @@ module load cineca
 module load mdsplus/7.46-1 
 module load cmake/3.5.2
 
-export PKG_CONFIG_PATH=$HOME/public/uda/lib/pkgconfig
+export UDA_HOME=$HOME/public/uda
+export PKG_CONFIG_PATH=$UDA_HOME/lib/pkgconfig
 
 cmake -Bbuild -H. -DCMAKE_BUILD_TYPE=Debug \
-    -DCMAKE_INSTALL_PREFIX=$HOME/public/uda \
+    -DCMAKE_INSTALL_PREFIX=$UDA_HOME \
     -DBUILD_PLUGINS=imas_mapping\;hl2a\;tore_supra \
