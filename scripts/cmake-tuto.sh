6ba0828b702cef069daabe9142fa238ffea39124
Fleury <ludovic.fleury@cea.fr>
2019-01-14 10:39:16 +0100

    Adding gcc module to cmake script file

--- scripts/cmake-tuto.sh
+++ scripts/cmake-tuto.sh
@@ -2,8 +2,9 @@
 
 module purge
 module load cineca
-module load mdsplus/7.46-1 
 module load cmake/3.5.2
+module load itm-gcc/6.1.0 
+module load mdsplus/7.46-1 
 
 export UDA_HOME=$HOME/public/uda
 export PKG_CONFIG_PATH=$UDA_HOME/lib/pkgconfig
