ed2e6b48f33784bd9015c7e5238b794d360bc771
LF218007 <ludovic.fleury@cea.fr>
2019-04-12 11:06:51 +0200

    soft_x_rays calibration

--- source/west/west_soft_x_rays.c
+++ source/west/west_soft_x_rays.c
@@ -27,11 +27,20 @@ float second_point_z[45] =  {-654.80,-623.10,-591.70,-560.50,-529.60,-498.90,-46
 		560.50,591.70,623.10,654.80};
 
 int channels_power_density(int shotNumber, char* nomsigp, int extractionIndex, float** time, float** data, int* len);
+void apply_calibration( float* calibrated_data, float* data, float Cx, float Ce, float E, int len);
 
 void soft_x_rays_throwsIdamError(int status, char* methodName, char* object_name, int index, int shotNumber) {
 	int err = 901;
 	char msg[1000];
 	sprintf(msg, "%s(%s),object:%s,index:%d,shot:%d,err:%d\n", "WEST:ERROR", methodName, object_name, index, shotNumber, status);
+	//UDA_LOG(UDA_LOG_ERROR, "%s", msg);
+	addIdamError(CODEERRORTYPE, msg, err, "");
+}
+
+void soft_x_rays2_throwsIdamError(int status, char* methodName, char* object_name, int shotNumber) {
+	int err = 901;
+	char msg[1000];
+	sprintf(msg, "%s(%s),object:%s,shot:%d,err:%d\n", "WEST:ERROR", methodName, object_name, shotNumber,status);
 	//UDA_LOG(UDA_LOG_ERROR, "%s", msg);
 	addIdamError(CODEERRORTYPE, msg, err, "");
 }
@@ -126,18 +135,84 @@ int soft_x_rays_channels_power_density_data(int shotNumber, DATA_BLOCK* data_blo
 	UDA_LOG(UDA_LOG_DEBUG, "reading channels_power_density...\n");
 	int status = channels_power_density(shotNumber, nomsigp, extractionIndex, &time, &data, &len);
 
+	UDA_LOG(UDA_LOG_DEBUG, "setting file pointer...\n");
+	FILE* p_file_soft_x_rays_calib_ce_cx_e;
+
+	char content[50];
+	float* Ce = malloc(CHANNELS_COUNT*sizeof(float));
+	float* Cx = malloc(CHANNELS_COUNT*sizeof(float));
+	float* E = malloc(CHANNELS_COUNT*sizeof(float));
+
+	const int WIDTH = 3;
+
+	UDA_LOG(UDA_LOG_DEBUG, "reading WEST_SOFT_X_RAYS_CALIB_CE_CX_E_FILE...\n");
+	char* soft_x_rays_calib_ce_cx_e_file = getenv("WEST_SOFT_X_RAYS_CALIB_CE_CX_E_FILE");
+	p_file_soft_x_rays_calib_ce_cx_e = fopen(soft_x_rays_calib_ce_cx_e_file, "r");
+
+	if (p_file_soft_x_rays_calib_ce_cx_e == NULL) {
+		soft_x_rays2_throwsIdamError(status, "readCalibrationFile", "unable to read soft_x_rays calibration file", shotNumber);
+		return status;
+	} else {
+		int i = 0;
+		int j = 0;
+		/*while (!feof(p_file_soft_x_rays_calib_ce_cx_e)) {
+			if (fgets(content, sizeof(content), p_file_soft_x_rays_calib_ce_cx_e) != NULL) {
+				const char delim[] = " ";
+				UDA_LOG(UDA_LOG_DEBUG, "reading coefficient Ce[%d]\n", i);
+				//char * c = strtok(content, delim);
+				//UDA_LOG(UDA_LOG_DEBUG, "c = %s\n", c);
+				Ce[i] = atof(strtok(content, delim)); //the Ce coefficient
+				UDA_LOG(UDA_LOG_DEBUG, "Ce[%d] = %f\n", i, Ce[i]);
+				UDA_LOG(UDA_LOG_DEBUG, "reading coefficient Cx[%d]\n", i);
+				Cx[i] = atof(strtok(NULL, delim)); //the Cx coefficient
+				UDA_LOG(UDA_LOG_DEBUG, "Cx[%d] = %f\n", i, Cx[i]);
+				UDA_LOG(UDA_LOG_DEBUG, "reading coefficient E[%d]\n", i);
+				E[i]  = atof(strtok(NULL, delim)); //the E coefficient
+				UDA_LOG(UDA_LOG_DEBUG, "E[%d] = %f\n", i, E[i]);
+				i++;
+			}
+			UDA_LOG(UDA_LOG_DEBUG, "test1\n", i);
+		}*/
+
+		 for(i = 0; i < CHANNELS_COUNT; i++)
+		  {
+		   /* for (j = 0 ; j < WIDTH; j++)
+		    {
+		      fscanf(p_file_soft_x_rays_calib_ce_cx_e,"%lf",&myvariable);
+		      printf("%.15f ",myvariable);
+		    }
+		    printf("\n");*/
+			 fscanf(p_file_soft_x_rays_calib_ce_cx_e,"%e",&Ce[i]);
+			 UDA_LOG(UDA_LOG_DEBUG, "Ce[%d] = %e\n", i, Ce[i]);
+			 fscanf(p_file_soft_x_rays_calib_ce_cx_e,"%e",&Cx[i]);
+			 UDA_LOG(UDA_LOG_DEBUG, "Cx[%d] = %e\n", i, Cx[i]);
+			 fscanf(p_file_soft_x_rays_calib_ce_cx_e,"%e",&E[i]);
+			 UDA_LOG(UDA_LOG_DEBUG, "E[%d] = %e\n", i, E[i]);
+		  }
+
+		UDA_LOG(UDA_LOG_DEBUG, "closing file\n");
+		fclose(p_file_soft_x_rays_calib_ce_cx_e);
+	}
+	UDA_LOG(UDA_LOG_DEBUG, "allocation of calibrated_data\n");
+	float *calibrated_data = malloc(len*sizeof(float));
+	UDA_LOG(UDA_LOG_DEBUG, "applying calibration coefficients\n");
+	apply_calibration(calibrated_data, data, Cx[index - 1], Ce[index - 1], E[index - 1], len);
+	UDA_LOG(UDA_LOG_DEBUG, "after applying calibration coefficients\n");
 	if (status != 0) {
 		UDA_LOG(UDA_LOG_DEBUG, "reading channels_power_density, error status...\n");
 		soft_x_rays_throwsIdamError(status, "soft_x_rays_channels_power_density_data", nomsigp, extractionIndex, shotNumber);
 		free(time);
 		free(data);
+		free(calibrated_data);
 		return status;
 	}
 	else {
 		UDA_LOG(UDA_LOG_DEBUG, "setting channels_power_density...\n");
-		setReturnData2DFloat(data_block, 1, len, data);
+		setReturnData2DFloat(data_block, 1, len, calibrated_data);
+		//setReturnData2DFloat(data_block, 1, len, data);
 		return 0;
 	}
+	return 0;
 }
 
 int soft_x_rays_channels_power_density_time(int shotNumber, DATA_BLOCK* data_block, int* nodeIndices)
@@ -180,6 +255,17 @@ int channels_power_density(int shotNumber, char* nomsigp, int extractionIndex, f
 	addExtractionChars(nomsigp_to_extract, nomsigp, extractionIndex); //Concatenate nomsigp_to_extract avec !extractionIndex, example: !1, !2, ...
 	int rang[2] = { 0, 0 };
 	int status = readSignal(nomsigp_to_extract, shotNumber, 0, rang, time, data, len);
-	UDA_LOG(UDA_LOG_DEBUG, "end of reading channels_power_density signal...\n");
+	UDA_LOG(UDA_LOG_DEBUG, "end of reading channels_power_density signal !...\n");
 	return status;
+}
+
+void apply_calibration( float* calibrated_data, float* data, float Cx, float Ce, float E, int len) {
+	float Ri = 3.76;
+	float Ga = 4;
+	float Gr = 250997;
+	float C = 6553.6;
+	int i;
+	for (i = 0; i < len; i++) {
+		calibrated_data[i] = data[i]*Ri*Cx*Ce/Ga/Gr/C/E;
+	}
 }
