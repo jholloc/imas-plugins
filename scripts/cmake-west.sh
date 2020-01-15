44e8cbdbe7c71a1721484cee66e85d680bf9d25d
LF218007 <ludovic.fleury@cea.fr>
2020-01-15 10:38:36 +0100

    Updating cmake-west.sh

--- scripts/cmake-west.sh
+++ scripts/cmake-west.sh
@@ -5,15 +5,14 @@ module purge
 module use /Applications/Modules/soft
 module use /Applications/Modules/compilers
 module load gcc/6.4.0
-module load mdsplus/7.46-4 
+module load mdsplus/7.92-0 
 module load cmake/3.7.2
 module load blitz
 
 export BOOST_ROOT=/Applications/boost_1_66_0
-export UDA_HOME=/work/imas/uda
 export PKG_CONFIG_PATH=$UDA_HOME/lib/pkgconfig
 
 cmake -Bbuild -H. \
     -DCMAKE_INSTALL_PREFIX=$UDA_HOME \
-    -DBUILD_PLUGINS=imas_mapping\;west \
+    -DBUILD_PLUGINS=imas_mapping\;west\
     -DLIBTS_ROOT=/Home/devarc/PortageMatlab7/tslib_client2013 \
