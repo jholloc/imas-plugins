de557984df11eb886621e1804b5fea7d02b87e6a
Stephen Dixon <g2sdixon@r054c06s01.galileo.cineca.it>
2022-04-19 12:08:47 +0200

    creating release branch

--- source/west_tunnel/west_tunnel.c
+++ source/west_tunnel/west_tunnel.c
@@ -119,6 +119,9 @@ int west_tunnel(IDAM_PLUGIN_INTERFACE* idam_plugin_interface)
 		nanosleep(&sleep_for, NULL);
 
 		init = 1;
+                if (STR_IEQUALS(request_block->function, "init") || STR_IEQUALS(request_block->function, "initialise")){
+                    return 0;
+                }
 	}
 
 	if (STR_IEQUALS(request_data->function, "help")) {
